f609ba180872439f5607eb25e7823312034e57bc
thinkpadmaster <a.scerba02@gmail.com>
2023-07-14 00:48:43 -0500

    Rename to postDeploy and add file moving functionality

--- md/postDeploy.sh
+++ md/postDeploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # converts md to .tmpl.html with pandoc
+# Usage: ./postDeploy.sh title_of_post.md type
+# type can be 'blog' or 'projects' in my case
 
-readonly POSTS_DIR="../../html/posts"
+readonly POSTS_DIR="../html/$2"
 readonly POST_TEMP="POST_TEMPLATE.tmpl.html"
 
 post=$(basename "$1" .md) 
 echo "converting $post to HTML"
+echo "Outputting at '$POSTS_DIR/$post.tmpl.html'"
 
 # pandoc for md to html
 pandoc -f markdown-auto_identifiers -t html --template=$POST_TEMP --wrap=none -o "$POSTS_DIR/$post.tmpl.html" "$post".md
@@ -15,3 +18,7 @@ pandoc -f markdown-auto_identifiers -t html --template=$POST_TEMP --wrap=none -o
 
 # makes http & https links have target _blank and rel noopener noreferrer
 perl -i -0pe 's/(<\W*a\W*[^>]*href=)(["'"'"']http[s]?:\/\/[^"'"'"'>]*["'"'"'])([^>]*>)/$1$2 target="_blank" rel="noopener noreferrer"$3/g' "$POSTS_DIR/$post.tmpl.html"
+
+echo "moving $1 to 'deployed' folder"
+mkdir -p "deployed/$post"
+mv $1 "deployed/$post/$1"
